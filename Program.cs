c956b92e064a12ab770e0364cf13b738dab9de39
Foundy <foundy@foundy.io>
2020-06-09 12:29:52 +0900

    샘플플 및 디버깅 설정 추가

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace netcore_sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name?");
+
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
